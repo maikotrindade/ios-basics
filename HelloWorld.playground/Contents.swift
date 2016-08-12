757f834f3ff8fb660ff6438d7bd58513a228fc68
ifsp <ifsp@CTI-IFSPs-Mac-mini-39.local>
2016-08-12 20:00:59 -0300

    add initial commit

---
+++ HelloWorld.playground/Contents.swift
@@ -0,0 +1,29 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+
+var myVariable = 123    //variable
+let myConstant = 123456 //constant
+
+let anExplicit : Int = 2
+
+myVariable+=1000
+
+//myConstant = 2 //Cannot assing a value to a constant
+
+var someVariable : Int
+//someVariable + = 4 // error: not initial value
+
+var sampleText = "Welcome!"
+print (sampleText)
+
+var anOptionalInt : Int? = nil
+anOptionalInt = 55
+
+
+
+
+
